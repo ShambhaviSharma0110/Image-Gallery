6508553fa0e5ee635a109f44242873b9ae678c67
Shambhavi Sharma <60618802+ShambhaviSharma0110@users.noreply.github.com>
2021-03-27 18:22:24 +0530

    final update

--- img-gallery/src/App.js
+++ img-gallery/src/App.js
@@ -1,9 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import Img from './Components/Img';
+import ImgSearch from './Components/ImgSearch';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=20884971-d4d5f1a7acbd300a8b6ca5da4&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <>
-   
-    </>
+     <div className="container mx-auto">
+      <ImgSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <Img key={image.id} image={image} />
+        ))}
+      </div>}
+    </div>
   );
 }
 
